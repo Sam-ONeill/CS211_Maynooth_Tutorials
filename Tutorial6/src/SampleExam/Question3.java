6cf607f929fe9f9a95aa3865ef2a784c5bcf75b0
Sam-ONeill <samoneill500@gmail..com>
2023-05-11 15:51:26 +0100

    Tutorial 6

--- Tutorial6/src/SampleExam/Question3.java
+++ Tutorial6/src/SampleExam/Question3.java
@@ -3,10 +3,6 @@ import java.util.Arrays;
 
 public class Question3 {
 
-        public static void main(String[] args) {
-
-        }
-
         public static int[] createHashTable(int[] keys) {
             int[] hashTable = new int[11];
             Arrays.fill(hashTable, -1); // Initialize all entries with -1
